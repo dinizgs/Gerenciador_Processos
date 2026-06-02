1f0b341b6c55b75c31ac4588e7a2de080aa2b5e2
Gabriel Sidrim <gsidrimdiniz@gmail.com>
2026-06-02 11:26:08 -0300

    adicionando tempo de execução no reader

--- core/reader.py
+++ core/reader.py
@@ -7,3 +7,24 @@ from datetime import datetime
 #Função para retornar o usuário logado
 def obter_usuario():
     return pwd.getpwuid(os.getuid()).pw_name
+
+
+def calcular_tempo_execucao(dados_sobraram):
+    try:
+        utime = dados_sobraram[11]
+        stime = dados_sobraram[12]
+
+        total_ticks = utime +stime
+
+        ticks_por_segundo = os.sysconf(os.sysconf_names['SC_CLKTCK '])
+
+        segundos_totais = int(total_ticks / ticks_por_segundo)
+
+        horas = segundos_totais / 3600
+        minutos = (segundos_totais & 3600) // 60
+        segundos = segundos_totais & 60
+
+        return f"{horas:2d}:{minutos:2d}:{segundos:2d}"
+    
+    except Exception:
+        return "00:00:00"
