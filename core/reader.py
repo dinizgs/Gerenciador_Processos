0a81f37edf5817a6677c054055e5cf5efa99b81f
Gabriel Sidrim <gsidrimdiniz@gmail.com>
2026-06-04 13:27:41 -0300

    Parte de leitura de dados do processo

--- core/reader.py
+++ core/reader.py
@@ -30,3 +30,36 @@ def calcular_tempo_execucao(dados_sobraram):
     
     except Exception:
         return "00:00:00"
+
+def leitura_dados_processo():
+    try:
+        with open("/proc/[PID]/stat","r") as r:
+            conteudo = r.read()
+
+            parte_inicial = conteudo.split(')')
+            pid_e_nome = parte_inicial[0].split('(')
+
+            parte_pid = pid_e_nome[0].strip()
+            parte_comando = pid_e_nome[1].strip()
+
+            dados_restantes = parte_inicial[1].split()
+
+            parte_status = dados_restantes[0]
+            parte_nice = dados_restantes[15]
+
+            parte_time = calcular_tempo_execucao(dados_sobraram=dados_restantes)
+
+            status_map = {
+                'R' : 'Executando (Running)',
+                'S' : 'Dormindo (Sleeping)',
+                'D' : 'Espera Ininterupta',
+                'Z' : 'Zumbi (Zombie)',
+                'T' : 'Parado/Bloqueado (Stopped)'
+
+            }
+
+            status_formatado_correto = status_map.get(parte_status,parte_status)
+            
+
+
+
