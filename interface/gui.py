c2cfea1dbdff9cdf92bd92bb1e959ce7c5d890c5
Gabriel Sidrim <gsidrimdiniz@gmail.com>
2026-06-08 10:10:22 -0300

    Esqueleto inicial da interface

--- interface/gui.py
+++ interface/gui.py
@@ -1,5 +1,24 @@
+import sys
 from PySide6.QtWidgets import (
     QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QTableWidgetItem,
     QPushButton, QLineEdit, QLabel, QDialog, QSpinBox, QMessageBox, QDialogButtonBox,
-    QFormLayout, QTextEdit, QStatusBar
+    QFormLayout, QTextEdit, QStatusBar, QApplication
 )
+
+class Interface(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Gerenciador Processos Linux" )
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    janela = Interface()
+    janela.show()
+    sys.exit(app.exec())
+
+    
+
